a1aa2b9a1ef8c4d9cc6f2008b16a870e62d0ce35
Aaron Perrine <avsp.here@gmail.com>
2019-10-04 08:17:46 -0700

    starting pattern theory. syncing bf switch to new vm

--- level_10_yield/Program.cs
+++ level_10_yield/Program.cs
@@ -36,10 +36,6 @@ namespace level_10_yield
         {
             public int Value { get; set; }
 
-            // public Box(int _value)
-            // {
-            //     Value = _value;
-            // }
         }
 
         static void Example1_MultipleIterations()
@@ -74,9 +70,104 @@ namespace level_10_yield
              */
 
         }
+
+        static void Example2_InfiniteLoop()
+        {
+            IEnumerable<Box> GetInfiniteBoxes()
+            {
+                while(true)
+                {
+                    yield return new Box { Value = 5 };
+                }
+            }
+
+            foreach( Box b in GetInfiniteBoxes().Take(10) )
+                Console.WriteLine(b.Value);
+
+            //even though we have an infinite loop recall that we aren't actually running the loop until explictly called
+            //and when we are explictly calling we are only iterating over it 10 times
+        }
+
+        static void Example3_IRL_CustomIteration()
+        {
+            //The idea here is simply that we do not have to create an aux list to hold the data and only have to iterate once
+            //This is in-fact how linq is written
+            IEnumerable<int> WhereGt5 (List<int> ints)
+            {
+                foreach( int i in ints )
+                {
+                    if ( i > 5 )
+                        yield return i;    
+                }
+            }
+
+            foreach( int i in WhereGt5( new List<int> { 3,4,5,6,7,8} ) )
+                Console.WriteLine(i);
+        }
+
+
+        static void Example4_IRL_StatefulIteration()
+        {
+            //The method containing yield will be paused after it stops iterating (ie you take 5 from a 10 list, when it comes back it starts on #6)
+            //Thus the state is maintained
+            //This seems kind of obvious
+            IEnumerable<int> ShowingStateMaintain(List<int> ints)
+            {
+                int total = 0;
+                foreach( int i in ints )
+                {
+                    total += i;
+                    yield return total;
+                }
+            }
+            var yieldedList = ShowingStateMaintain( new List<int> { 1,2,3,4,5 } );
+            Console.WriteLine(yieldedList.Take(1).Last() );
+            Console.WriteLine(yieldedList.Take(2).Last() ); // now it is 3 since we resumed execution
+        }
+
+        static void Example5_IRL_DeferredExecution()
+        {
+            //This I see as the nice example
+            IEnumerable<Box> GetBoxes()
+            {
+                for( int i = 3; i < 10000; i++ )
+                {
+                    yield return new Box { Value = i };
+                }
+            }
+
+            string ValueToColor(int v)
+            {
+                return v < 5 ? "blue" : "green";
+            }
+
+            //Get boxes is ~10000 long
+            //This allows us to just take 5 rather than all 10000
+
+            //REMEMBER THAT THIS IS NOT A TRANSDUCER REPLACEMENT
+
+            var colors = GetBoxes()
+                .Take(5)
+                .Select( b => b.Value )
+                .OrderByDescending( v => v )
+                .Take(4)
+                .Select( v => ValueToColor(v) );
+
+            foreach( string c in colors )
+                Console.WriteLine(c);
+
+        }
+
         static void Main(string[] args)
         {
-            Example1_MultipleIterations();
+            // Example1_MultipleIterations();
+            // Example2_InfiniteLoop();
+            /*
+                IRL there only a few supposed main use cases (besides being able to get away with anotherwise infinite loop)
+            */
+            // Example4_IRL_StatefulIteration();
+            Example5_IRL_DeferredExecution();
+
         }
     }
 }
---
+++ level_11_abstractFactory/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace level_11_abstractFactory
+{
+    class Program
+    {
+        public interface IAirConditioner
+        {
+            void Operate();
+        }
+
+        //These Cooling and warming classes are just implementing our interface
+        public class Cooling : IAirConditioner
+        {
+            private readonly double _temperature;
+            
+            public Cooling(double temperature)
+            {
+                _temperature = temperature;
+            }
+
+            public void Operate()
+            {
+                Console.WriteLine("Cooling to {0} beep boop", _temperature);
+            }
+
+        }
+
+        public class Warming : IAirConditioner
+        {
+            private readonly double _temperature;
+
+            public Warming( double temperature ) 
+            {
+                _temperature = temperature;
+            }
+
+            public void Operate()
+            {
+                Console.WriteLine("Warming to {0} beep boop", _temperature);
+            }
+
+        }
+
+        //Now we create a factor to create these objects
+        public abstract class AirConditionerFactory
+        {
+            public abstract IAirConditioner Create(double temperature); //this class creating abstraction method...
+        }
+
+        public class CoolingFactory : AirConditionerFactory
+        {
+            public override IAirConditioner Create(double temperature) => new Cooling(temperature);
+        }
+        public class WarmingFactory : AirConditionerFactory
+        {
+            public override IAirConditioner Create(double temperature) => new Warming(temperature);
+        }
+
+        public enum Actions
+        {
+            Cooling,
+            Warming
+        }
+        
+        //now I define the actual AirConitioner Class
+
+
+        public class AirConditioner
+        {
+            private readonly Dictionary<Actions, AirConditionerFactory> _factories;
+
+            private AirConditioner()
+            {
+                _factories = new Dictionary<Actions, AirConditionerFactory>
+                {
+                    { Actions.Cooling, new CoolingFactory() },
+                    { Actions.Warming, new WarmingFactory() }
+                };
+
+            }
+
+            public static AirConditioner InitializeFactories() => new AirConditioner();
+
+            public IAirConditioner ExecuteCreation(Actions action, double temperature) => _factories[action].Create(temperature);
+        }
+
+
+        static void Main(string[] args)
+        {
+            /* FIRST EXAMPLE */
+
+            // AirConditioner
+            // .InitializeFactories()
+            // .ExecuteCreation(Actions.Cooling, 22.5)
+            // .Operate();
+
+            /* SECOND EXAMPLE */
+                        
+
+        }
+    }
+}
